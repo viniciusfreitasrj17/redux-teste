00c38d8ce4267b1af54b8e6fa0efa9b15e372e89
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-06-05 18:09:31 -0300

    Add Redux DevTools Extension

--- src/store/index.js
+++ src/store/index.js
@@ -47,6 +47,6 @@ const reducer = (state = INITIAL_STATE, action) => {
   return state;
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store
